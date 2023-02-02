2d782e0aa49a49bf70ffc24300c6b783ef2a63fb
GabrielWolf-Dev <65361452+GabrielWolf-Dev@users.noreply.github.com>
2023-02-02 11:42:53 -0300

    feat: Add TaskList component with data structures API

---
+++ src/components/TaskList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <fieldset v-for="task in tasks" :key="task.id">
+      <label :for="task.id">{{ task.value }}</label>
+      <input
+        type="checkbox"
+        :name="task.task"
+        :id="task.id"
+        :value="task.value"
+        :checked="task.completed"
+      />
+    </fieldset>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskList",
+  props: ["tasks"],
+};
+</script>
+
+<style scoped></style>
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -3,19 +3,38 @@
     Hello world!
     <InputText name="task" id="task" placeholder="Adicionar uma tarefa" />
     <ButtonSubmit text="Adicionar" />
+    <TaskList :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import ButtonSubmit from "@/components/ButtonSubmit.vue";
 import InputText from "@/components/InputText.vue";
+import TaskList from "@/components/TaskList.vue";
 
 export default {
   name: "TodosView",
   components: {
     ButtonSubmit,
     InputText,
-  }
+    TaskList,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          value: "Fazer exercícios",
+          completed: true,
+        },
+        {
+          id: 2,
+          value: "Fazer cursos",
+          completed: false,
+        },
+      ],
+    };
+  },
 };
 </script>
 
